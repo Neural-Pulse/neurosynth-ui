5f1d3bd69b1b56ca3dc8a56bdb99e6a3b2e3d3f1
Daniel Malagurti <danielmalagurti@gmail.com>
2023-11-07 15:15:09 -0300

    style: new color palettes apply to NestedList

--- src/components/NestedList/index.tsx
+++ src/components/NestedList/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { NestedListProps } from './NestedList.types';
 
-
 const NestedList: React.FC<NestedListProps> = ({ items }) => {
     return (
         <ul className="list-none p-0">
             {items.map((item, index) => (
-                <li key={index} className="cursor-pointer px-3 py-2 hover:bg-gray-200">
+                <li key={index} className="cursor-pointer px-3 py-2 rounded-xl shadow-green-glow-md border border-transparent hover:border-neuralpulse-green">
                     {item.title}
                     {item.subItems && item.subItems.length > 0 && (
                         <ul className="pl-4">
                             {item.subItems.map((subItem, subIndex) => (
-                                <li key={subIndex} className="cursor-pointer px-3 py-2 hover:bg-gray-300">
+                                <li key={subIndex} className="cursor-pointer px-3 py-2 hover:bg-neuralpulse-light-gray rounded-xl shadow-green-glow-md border border-transparent hover:border-neuralpulse-light-green">
                                     {subItem.title}
                                     {subItem.subItems && <NestedList items={subItem.subItems} />}
                                 </li>
