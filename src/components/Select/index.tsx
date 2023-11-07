2cc72b202a05a672b19f632cc525ca46d2b8f613
Daniel Malagurti <danielmalagurti@gmail.com>
2023-11-07 15:15:24 -0300

    style: new color palette apply to Select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -4,9 +4,9 @@ import { SelectProps } from './Select.types';
 const Select: React.FC<SelectProps> = ({ label, options, ...rest }) => {
   return (
     <div className="flex flex-col">
-      <label className="mb-2 font-bold text-dark-gray">{label}</label>
+      <label className="mb-2 font-bold text-neuralpulse-dark">{label}</label>
       <select
-        className="border border-light-gray rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-cyan-blue"
+        className="border border-neuralpulse-gray rounded-sm p-2 focus:outline-none focus:ring-2 focus:ring-neuralpulse-accent shadow-green-glow-md"
         {...rest}
       >
         {options.map((option) => (
