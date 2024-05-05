92309f10c4cb6995bbeb5be0535dabc80b1046f2
Daniel Malagurti <danielmalagurti@gmail.com>
2024-05-05 09:13:31 -0300

    fix: fix tests to deploy

--- src/components/PasswordField/index.tsx
+++ src/components/PasswordField/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { PasswordFieldProps } from './PasswordField.types';
 
 const PasswordField: React.FC<PasswordFieldProps> = ({
