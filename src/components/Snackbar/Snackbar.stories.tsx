53b7a56a44e228032f6179fa3098e52d7ae68056
Daniel Malagurti <danielmalagurti@gmail.com>
2024-05-05 15:48:18 -0300

    feat: implement Snackbar

---
+++ src/components/Snackbar/Snackbar.stories.tsx
@@ -0,0 +1,16 @@
+import { StoryFn, Meta } from '@storybook/react';
+import Snackbar from '.';
+import { SnackbarProps } from './Snackbar.types';
+
+export default {
+    title: 'Components/Snackbar',
+    component: Snackbar,
+} as Meta;
+
+const Template: StoryFn<SnackbarProps> = (args) => <Snackbar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    message: 'This is a snackbar message!',
+    duration: 3000
+};
