ba3ce1a14c78d228e6b9e679eb2f09264f503971
Daniel Malagurti <danielmalagurti@gmail.com>
2024-05-13 09:50:57 -0300

    refactor: :art: improve Select component

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,20 +1,39 @@
+
+import React from 'react';
 import { StoryFn, Meta } from '@storybook/react';
 import Select from '.';
-import { SelectProps } from './Select.types';
 
 export default {
     title: 'Components/Select',
     component: Select,
-} as Meta;
+} as Meta<typeof Select>;
 
-const Template: StoryFn<SelectProps> = (args) => <Select {...args} />;
+const Template: StoryFn<typeof Select> = (args) => <Select {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    label: 'Escolha uma opção',
+    id: 'default-select',
+    label: 'Choose an option',
     options: [
-        { value: 'option1', label: 'Opção 1' },
-        { value: 'option2', label: 'Opção 2' },
-        { value: 'option3', label: 'Opção 3' },
+        { value: 'option1', label: 'Option 1' },
+        { value: 'option2', label: 'Option 2' },
+        { value: 'option3', label: 'Option 3' }
+    ],
+};
+
+export const CustomTheme = Template.bind({});
+CustomTheme.args = {
+    id: 'custom-theme-select',
+    label: 'Custom Theme Select',
+    theme: {
+        labelColor: 'text-red-500',
+        borderColor: 'border-green-500',
+        focusRingColor: 'focus:ring-blue-500',
+        shadow: 'shadow-lg'
+    },
+    options: [
+        { value: 'option1', label: 'Option 1' },
+        { value: 'option2', label: 'Option 2' },
+        { value: 'option3', label: 'Option 3' }
     ],
 };
