ec85269059e864341a2c7dfdd8c0f17fc3e6e442
Daniel Malagurti <danielmalagurti@gmail.com>
2024-06-16 11:51:28 -0300

    style: :sparkles: improve flexibility of Tooltip

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,36 +1,43 @@
-import { StoryFn, Meta } from "@storybook/react";
-import Tooltip from ".";
-import Button from "../Button";
+// src/components/Tooltip/Tooltip.stories.tsx
+
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import Tooltip from './'; // Certifique-se de importar corretamente o Tooltip
+import { TooltipProps } from './Tooltip.types';
 
 export default {
-  title: "Components/Tooltip",
+  title: 'Components/Tooltip',
   component: Tooltip,
-} as Meta<typeof Tooltip>;
+} as Meta;
 
-const Template: StoryFn<typeof Tooltip> = (args) => <Tooltip {...args} />;
-
-export const Top = Template.bind({});
-Top.args = {
-  message: "Tooltip on Top",
-  position: "top",
-  children: (
-    <Button
-      variant="text"
-      label="Hover me"
-      onClick={() => alert("Button clicked!")}
-    />
-  ),
-};
+const Template: StoryFn<TooltipProps> = (args) => (
+  <div className="flex items-center justify-center h-screen">
+    <Tooltip {...args}>
+      <button className="p-2 bg-blue-500 text-white rounded">Hover me</button>
+    </Tooltip>
+  </div>
+);
 
 export const Bottom = Template.bind({});
 Bottom.args = {
-  message: "Tooltip on Bottom",
-  position: "bottom",
-  children: (
-    <Button
-      variant="text"
-      label="Hover me"
-      onClick={() => alert("Button clicked!")}
-    />
-  ),
+  message: 'Tooltip on bottom',
+  position: 'bottom',
+};
+
+export const Top = Template.bind({});
+Top.args = {
+  message: 'Tooltip on top',
+  position: 'top',
+};
+
+export const Left = Template.bind({});
+Left.args = {
+  message: 'Tooltip on left',
+  position: 'left',
+};
+
+export const Right = Template.bind({});
+Right.args = {
+  message: 'Tooltip on right',
+  position: 'right',
 };
